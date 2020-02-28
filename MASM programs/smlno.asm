adb0a751029a1dd5a382bb4d527fe1d05a62e2d8
Abhicake <46848605+Abhicake@users.noreply.github.com>
2020-02-28 07:57:49 +0530

    Update smlno.asm

--- MASM programs/smlno.asm
+++ MASM programs/smlno.asm
@@ -1,1 +1,31 @@
 ; smallest no.
+
+Assume cs:code , ds:data
+data segment
+data ends
+code segment
+start:
+	mov ax,0ffffh;
+	mov si , 0200h;
+	mov cx,[si]
+	mov cx,[si];
+back:
+	inc si;
+	inc si
+	cmp ax,[si]
+	JB go
+	mov ax,[si]
+
+go:
+	loop back
+	mov di,0251
+	mov [di],ax;
+	;mov [di+1] , ah;
+	int 03h
+	
+	code ends
+	end start
+	
+; o/p:
+; ax = smallest no.
+	
